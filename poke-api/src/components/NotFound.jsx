ed9dfb79b379fcc77b456391e5e0c92320693f39
BigDickMilan <milantheunissen2003@gmail.com>
2023-06-15 16:26:57 +0200

    Update NotFound.jsx

--- poke-api/src/components/NotFound.jsx
+++ poke-api/src/components/NotFound.jsx
@@ -6,6 +6,7 @@ function NotFound() {
       <h1>404 Not Found</h1>
       <h2>weg gaan hier melvin deze pagina bestaat niet.</h2>
       <h2>Enge errors</h2>
+      <h2>dit is een geupdate versie van routing vergeleken met vorig project omdat ik deze zin heb toegevoegd</h2>
     </div>
   );
 }
